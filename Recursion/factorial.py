fd60b2e46be8d66d5a80d510f569dc12733a6287
Sina Kalantar <sina1999.sk@gmail.com>
2022-06-14 15:55:38 -0700

    Update factorial.py
    code was revised and improved

--- Recursion/factorial.py
+++ Recursion/factorial.py
@@ -1,6 +1,5 @@
 def factorial(n):  #factorial of number n solved by recursion
-    if n == 0: #base condition 
-        return 1 #terminates recursion
-    x = n * factorial(n-1)  #calculates factorial of n by calling factorial() recursively 
-                            #to calcualte n.(n-1)! til base condition satisfied 
-    return x 
+    if n in [0,1]: #base conditions 0!,1! = 1 
+        return 1   #terminates recursion
+    return n * factorial(n-1)  #calculates factorial of n by calling factorial() recursively 
+                               #to calcualte n.(n-1)! til base condition satisfied 
