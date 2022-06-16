f6a46e021cd3d77b74d504b106d37788e047afb0
Sina Kalantar <sina1999.sk@gmail.com>
2022-06-16 16:24:04 -0700

    comment on recursive calls adjusted

--- Recursion/factorial.py
+++ Recursion/factorial.py
@@ -1,5 +1,5 @@
 def factorial(n):  #factorial of number n solved by recursion
-    assert int(n) == n and n >= 0, "n should only be a postive integer" #edge cases where inifinte loop occurs
+    assert int(n) == n and n >= 0, "n should only be a postive integer" #edge cases where inifinte loop occur
     if n in [0,1]: #base conditions 0!,1! = 1 
         return 1   #terminates recursion
     return n * factorial(n-1)  #calculates factorial of n by calling factorial() recursively 
--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -1,5 +1,5 @@
 def fibonacci(n):
-    assert n >= 0 and int(n) == n,"n should only be a positive integer" #edge cases where infinite loop occurs
+    assert n >= 0 and int(n) == n,"n should only be a positive integer" #edge cases where infinite loop occur
     if n ==0 : #first base condition, when n is 0 returns 0
         return 0
     elif n == 1 :#second base condition, when n is 1 returns 1
