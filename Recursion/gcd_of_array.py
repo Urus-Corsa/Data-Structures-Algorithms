c3d4d5aa8cd7cee4fa99d3fbc37039a1807d2904
Sina Kalantar <sina1999.sk@gmail.com>
2022-06-17 18:54:11 -0700

    comments added to gcd_of_array.py

--- Recursion/gcd_of_array.py
+++ Recursion/gcd_of_array.py
@@ -1,14 +1,14 @@
-def gcdOfArray(nums):
+def gcdOfArray(nums): #finds GCD of smallest and largest elements in given array of int
     i = 0
-    for i in range(nums[i]):
-        largest = max(nums)
-        smallest = min(nums)
+    for i in range(nums[i]): 
+        largest = max(nums) #finds largest element
+        smallest = min(nums) #finds smallest element
         break
-    remainder = largest%smallest 
-    if remainder == 0:
-        return smallest
-    elif  remainder == 1:
+    remainder = largest%smallest  
+    if remainder == 0: #base condition if smallest element itself is GCD between two elements
+        return smallest 
+    elif  remainder == 1: #base condition if remainder of division is 1, one is GCD 
         return 1
     else:    
-        return gcdOfArray([remainder,smallest])
+        return gcdOfArray([remainder,smallest]) #recursively call gcdOfArray on remainder and smallest until base condition met
 
