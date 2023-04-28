f0b9f4932d70e14af75230d1f71320bef5977f19
sinakalantar <sinakalantar@lucidmotors.com>
2023-04-28 14:08:46 -0700

    LC 14 solved

--- Strings/longestCommonPrefix.py
+++ Strings/longestCommonPrefix.py
@@ -1,1 +1,20 @@
-
+#LC 14
+def longestCommonPrefix(strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 1:
+            return strs[0]
+        shortest = min(strs)
+        for elem in strs:
+            if elem.startswith(shortest):
+                  continue
+            else:
+                  for j in range(len(shortest)):
+                        if elem.startswith(shortest[0 :-1-j]):
+                              shortest = shortest[0 :-1-j]
+                              break
+                        
+            continue
+        return shortest
