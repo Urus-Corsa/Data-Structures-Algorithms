762b5550c6167c30b015f76b3ced674ce5868c72
sinakalantar <sinakalantar@lucidmotors.com>
2023-05-02 08:39:07 -0700

    LC 58 solved

---
+++ Strings/lengthOfLastWord.py
@@ -0,0 +1,15 @@
+#LC 58
+def lola(self, s: str):
+    """
+    :type s: str
+    :rtype: int
+    """
+    if s.strip() == "":
+        return 0
+    reversed = s[::-1].strip()
+    for i in range(len(reversed)):
+        if reversed[i] == " ":
+            return i
+    return len(reversed)    
+
+
